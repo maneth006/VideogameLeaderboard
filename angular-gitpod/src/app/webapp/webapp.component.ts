a8d0fdbd32f61a6ced86e334441a62d8e8be6819
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-03-18 08:48:34 +0000

    1 commit

---
+++ angular-gitpod/src/app/webapp/webapp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-webapp',
+  standalone: true,
+  imports: [],
+  templateUrl: './webapp.component.html',
+  styleUrl: './webapp.component.css'
+})
+export class WebappComponent {
+
+}
