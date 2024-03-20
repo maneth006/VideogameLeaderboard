1f1430397a759ec306d6d7ee1fd40ed7c109bc99
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-03-20 19:06:30 +0000

    commit half img

--- angular-gitpod/src/app/webapp/webapp.component.ts
+++ angular-gitpod/src/app/webapp/webapp.component.ts
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './webapp.component.css'
 })
 export class WebappComponent {
+  persone = [
+    
+    {Nome_Utente: "Bugha", Nazionalità: "USA", eta: "21", ruolo: "Giocatore professionista Battle Royale", colore: "rgb(255, 255, 0)", img: "assets/img/Leao.jpg", link: "https://fortnite-esports.fandom.com/wiki/Veno"},
+ 
+    {Nome_Utente: "Queasy", Nazionalità: "Serbia", eta: "21", ruolo: "Giocatore professionista Battle Royale", colore: "rgb(255, 255, 0)", img: "assets/img/Queasy.png", link: "https://fortnite-esports.fandom.com/wiki/Queasy"},
+ 
+    {Nome_Utente: "Veno", Nazionalità: "England", eta: "19", ruolo: "Giocatore professionista Battle Royale", colore: "rgb(255, 255, 0)", img: "assets/img/Veno.png", link: "https://fortnite-esports.fandom.com/wiki/Veno"},
 
+    {Nome_Utente: "Setty", Nazionalità: "Poland", eta: "20", ruolo: "Giocatore professionista Battle Royale", colore: "rgb(255, 255, 0)", img: "assets/img/Setty.png", link: "https://fortnite-esports.fandom.com/wiki/Setty"},
+
+    {Nome_Utente: "Kami", Nazionalità: "Poland", eta: "19", ruolo: "Giocatore professionista Battle Royale", colore: "rgb(255, 255, 0)", img: "assets/img/Kami.png", link: "https://fortnite-esports.fandom.com/wiki/Kami"},
+ 
+    {Nome_Utente: "Bugha", Nazionalità: "USA", eta: "21", ruolo: "Giocatore professionista Battle Royale", colore: "rgb(255, 255, 0)", img: "assets/img/Leao.jpg", link: "https://fortnite-esports.fandom.com/wiki/Veno"},
+ 
+    {Nome_Utente: "Bugha", Nazionalità: "USA", eta: "21", ruolo: "Giocatore professionista Battle Royale", colore: "rgb(255, 255, 0)", img: "assets/img/Leao.jpg", link: "https://fortnite-esports.fandom.com/wiki/Veno"},
+
+    {Nome_Utente: "Bugha", Nazionalità: "USA", eta: "21", ruolo: "Giocatore professionista Battle Royale", colore: "rgb(255, 255, 0)", img: "assets/img/Leao.jpg", link: "https://fortnite-esports.fandom.com/wiki/Veno"},
+ 
+    {Nome_Utente: "Bugha", Nazionalità: "USA", eta: "21", ruolo: "Giocatore professionista Battle Royale", colore: "rgb(255, 255, 0)", img: "assets/img/Leao.jpg", link: "https://fortnite-esports.fandom.com/wiki/Veno"},
+
+    {Nome_Utente: "Bugha", Nazionalità: "USA", eta: "21", ruolo: "Giocatore professionista Battle Royale", colore: "rgb(255, 255, 0)", img: "assets/img/Leao.jpg", link: "https://fortnite-esports.fandom.com/wiki/Veno"},
+ 
+  ]
 }
